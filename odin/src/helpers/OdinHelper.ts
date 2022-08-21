4d940566200978ef5285168ca7f4c8a1219ea6b1
Ali Moussa <Aamoussa97@gmail.com>
2022-08-21 08:30:08 +0200

    Changed multiple files. Removed depdencies for puppeteer, playwright. Removed method retrieveAlarmsFromOdinSourceViaPlaywright. Standard is now HTTP GET.

--- odin/src/helpers/OdinHelper.ts
+++ odin/src/helpers/OdinHelper.ts
@@ -1,47 +1,9 @@
 import axios from "axios";
-import { chromium } from "playwright";
 import * as Cheerio from "cheerio";
 import {ODIN_HTML_ALARM_UPDATED_AT_REGEX, ODIN_HTML_ALARMS_TABLE_ID, ODIN_SOURCE_URL} from "../constants/OdinConstants";
 import { OdinAlarm } from "../models/OdinAlarm";
 
 export default class OdinHelper {
-    retrieveAlarmsFromOdinSourceViaPlaywright = async () => {
-        const browser = await chromium.launch({
-            headless: true
-        });
-
-        const context = await browser.newContext({
-            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54',
-            extraHTTPHeaders: {
-                'Host': 'www.odin.dk',
-                'Upgrade-Insecure-Requests': '1',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'da,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
-                'Cache-Control': 'max-age=0',
-                'Connection': 'keep-alive'
-            }
-        })
-
-        try {
-            const page = await browser.newPage();
-
-            await page.goto(ODIN_SOURCE_URL, {
-                waitUntil: 'load',
-            });
-
-            const content = await page.content();
-
-            return this.parseOdinAlarms(content);
-        } catch (error) {
-            console.log(error);
-        } finally {
-            if (browser) {
-                await browser.close();
-            }
-        }
-    };
-
     retrieveAlarmsFromOdinPuls = async () => {
         try {
             const response = await axios({
